eb2b7ccc1353b928637e88cc7e563be5313da70c
Michel Lamarlière <michel.lamarliere@icloud.com>
2021-05-10 16:19:11 +0200

    webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	entry: "./src/index.js",
 	output : {
 		filename: "main.js",
-		path: path.resolve(__dirname, "build"),
+		path: path.resolve(__dirname, "dist"),
         clean: true,
 	},
     module: {
